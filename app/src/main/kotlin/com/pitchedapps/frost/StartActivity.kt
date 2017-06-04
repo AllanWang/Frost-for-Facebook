e71e3ffdebbf42ede544d3d2b7584b4ed0116577
Allan Wang <me@allanwang.ca>
2017-06-03 21:47:07 -0700

    test crashlytics 2

--- app/src/main/kotlin/com/pitchedapps/frost/StartActivity.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/StartActivity.kt
@@ -2,11 +2,9 @@ package com.pitchedapps.frost
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.crashlytics.android.Crashlytics
 import com.pitchedapps.frost.dbflow.loadFbCookiesAsync
 import com.pitchedapps.frost.utils.L
 import com.pitchedapps.frost.utils.launchNewTask
-import io.fabric.sdk.android.Fabric
 
 /**
  * Created by Allan Wang on 2017-05-28.
@@ -15,7 +13,6 @@ class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Fabric.with(this, Crashlytics())
         L.d("Load cookies ${System.currentTimeMillis()}")
         loadFbCookiesAsync {
             cookies ->
