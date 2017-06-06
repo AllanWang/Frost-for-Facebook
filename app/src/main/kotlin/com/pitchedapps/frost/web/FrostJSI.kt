e3c6e09daadbe4fff07c5e047f9db1bd9eca1351
Allan Wang <me@allanwang.ca>
2017-06-05 23:40:14 -0700

    Remove eventbus bindings

--- app/src/main/kotlin/com/pitchedapps/frost/web/FrostJSI.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/web/FrostJSI.kt
@@ -1,13 +1,13 @@
 package com.pitchedapps.frost.web
 
 import android.webkit.JavascriptInterface
-import com.pitchedapps.frost.events.WebLaunchEvent
-import org.greenrobot.eventbus.EventBus
 
 /**
  * Created by Allan Wang on 2017-06-01.
  */
 class FrostJSI {
     @JavascriptInterface
-    fun loadUrl(url: String) = EventBus.getDefault().post(WebLaunchEvent(url))
+    fun loadUrl(url: String){
+
+    }
 }
