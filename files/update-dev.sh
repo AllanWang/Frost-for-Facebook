ab98d536da8cd5e98132e125b81dde7bfcfa8056
Allan Wang <me@allanwang.ca>
2017-07-04 00:32:01 -0700

    mist

--- files/update-dev.sh
+++ files/update-dev.sh
@@ -4,7 +4,7 @@ git checkout dev
 git branch -d dev-bak
 git checkout -b dev-bak
 git checkout master
-git pull origin master
+git pull -f origin master
 git branch -d dev
 git checkout -b dev
 git push --set-upstream origin dev
