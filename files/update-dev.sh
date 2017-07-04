caf3c4187706ba436fad5e3fa4fd1476a9a4e3ee
Allan Wang <me@allanwang.ca>
2017-07-03 22:56:18 -0700

    Update kau

---
+++ files/update-dev.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git checkout dev
+git branch -d dev-bak
+git checkout -b dev-bak
+git checkout master
+git pull origin master
+git branch -d dev
+git checkout -b dev
+git push --set-upstream origin dev
