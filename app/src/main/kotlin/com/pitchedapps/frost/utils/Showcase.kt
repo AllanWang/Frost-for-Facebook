ea3e354ae1641e7866caeefcd54f51c1e4b6c228
Allan Wang <me@allanwang.ca>
2017-07-03 22:15:04 -0700

    Create showcase prefs

---
+++ app/src/main/kotlin/com/pitchedapps/frost/utils/Showcase.kt
@@ -0,0 +1,16 @@
+package com.pitchedapps.frost.utils
+
+import ca.allanwang.kau.kpref.KPref
+import ca.allanwang.kau.kpref.kpref
+
+/**
+ * Created by Allan Wang on 2017-07-03.
+ *
+ * Showcase prefs that offer one time helpers to guide new users
+ */
+object Showcase : KPref() {
+
+    //check if this is the first time launching the web overlay; show snackbar if true
+    var firstWebOverlay: Boolean by kpref("first_web_overlay", true)
+}
+
