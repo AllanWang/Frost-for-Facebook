7ea21ee4884a69313e336aae7c7102ac79d6a6f8
Allan Wang <me@allanwang.ca>
2017-07-06 12:34:34 -0700

    Add exception to profile loading

--- app/src/main/kotlin/com/pitchedapps/frost/LoginActivity.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/LoginActivity.kt
@@ -118,6 +118,7 @@ class LoginActivity : BaseActivity() {
             }
 
             override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                if (e != null) L.e(e, "Profile loading exception")
                 profileObservable.onSuccess(false)
                 return false
             }
