d2851b58b2ec04ba8b582b524a102448efb48ce5
Allan Wang <me@allanwang.ca>
2017-08-10 16:21:08 -0700

    Create debugger

---
+++ app/src/main/kotlin/com/pitchedapps/frost/facebook/JsoupCleaner.kt
@@ -0,0 +1,33 @@
+package com.pitchedapps.frost.facebook
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Attribute
+import org.jsoup.nodes.Element
+import org.jsoup.safety.Whitelist
+
+/**
+ * Created by Allan Wang on 2017-08-10.
+ *
+ * Parses html with Jsoup and cleans the data, emitting just the frame containing debugging info
+ *
+ * Removes text, removes unnecessary nodes
+ */
+fun String.cleanHtml() = cleanText().cleanJsoup()
+
+internal fun String.cleanText(): String = replace(Regex(">.+?<"), "><")
+
+internal fun String.cleanJsoup(): String = Jsoup.clean(this, PrivacyWhitelist())
+
+class PrivacyWhitelist : Whitelist() {
+
+    val blacklistAttrs = arrayOf("style", "aria-label", "rel")
+    val blacklistTags = arrayOf("body", "html", "head", "i", "b", "u", "style", "br", "p", "span", "ul", "ol", "li")
+
+    override fun isSafeAttribute(tagName: String, el: Element, attr: Attribute): Boolean {
+        val key = attr.key
+        if (key == "href") attr.setValue("-")
+        return key !in blacklistAttrs
+    }
+
+    override fun isSafeTag(tag: String) = tag !in blacklistTags
+}
---
+++ app/src/test/kotlin/com/pitchedapps/frost/facebook/JsoupCleanerTest.kt
@@ -0,0 +1,50 @@
+package com.pitchedapps.frost.facebook
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+/**
+ * Created by Allan Wang on 2017-08-10.
+ */
+class JsoupCleanerTest {
+
+    val whitespaceRegex = Regex("\\s+")
+
+    fun String.cleanWhitespace() = replace("\n", "").replace(whitespaceRegex, " ").replace("> <", "><")
+
+    private fun String.assertCleanHtml(expected: String) {
+        assertEquals(expected.cleanWhitespace(), cleanHtml().cleanWhitespace())
+    }
+
+    private fun String.assertCleanJsoup(expected: String) {
+        assertEquals(expected.cleanWhitespace(), cleanJsoup().cleanWhitespace())
+    }
+
+    private fun String.assertCleanText(expected: String) {
+        assertEquals(expected.cleanWhitespace(), cleanText().cleanWhitespace())
+    }
+
+    @Test
+    fun noChange() {
+        "<a><aa> HI </aa></a>".assertCleanJsoup("<a><aa> HI </aa></a>")
+    }
+
+    @Test
+    fun basicText() {
+        """<div class="test">Hello world</div>""".assertCleanHtml("""<div class="test"></div>""")
+    }
+
+    @Test
+    fun textRemoval() {
+        """<div>Hello<a>World</a></div>""".assertCleanText("<div><a></a></div>")
+    }
+
+    @Test
+    fun kau() {
+        val html = """<div class="col s12 m6"> <div id="kau" class="card medium sticky-action"> <div class="card-image waves-effect waves-block waves-light"> <img class="activator" src="images/kau.jpg"> <span class="card-title activator background-gradient">KAU</span> </div><div class="card-content"><p>An extensive collection of Kotlin Android Utils</p></div><div class="card-action"> <a href="https://github.com/AllanWang/KAU" target="_blank" class="inline-block">Github</a> <a href="https://allanwang.github.io/KAU/" target="_blank" class="inline-block">Page</a> </div><div class="card-reveal"> <span class="card-title grey-text text-darken-4">KAU<i class="material-icons right">close</i></span> <ul class="browser-default"> <li>Huge package of one line extension functions</li><li>Custom UI views</li><li>Adapter items and animators</li><li>SearchView</li><li>Custom delegates</li></ul> </div></div></div>"""
+        val expected = """<div class="col s12 m6"><div id="kau" class="card medium sticky-action"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="images/kau.jpg"></div><div class="card-action"><a href="-" target="_blank" class="inline-block"></a><a href="-" target="_blank" class="inline-block"></a></div></div></div>"""
+        html.assertCleanHtml(expected)
+    }
+
+}
+
