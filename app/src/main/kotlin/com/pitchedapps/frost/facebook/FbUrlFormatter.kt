5b35bacfee666114dd19a62c76d3ddd1c34e0a7a
Allan Wang <me@allanwang.ca>
2017-09-24 02:00:58 -0400

    Split converter and decoder

--- app/src/main/kotlin/com/pitchedapps/frost/facebook/FbUrlFormatter.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/facebook/FbUrlFormatter.kt
@@ -10,7 +10,7 @@ import com.pitchedapps.frost.utils.L
 val String.formattedFbUrl: String
     get() = FbUrlFormatter(this).toString()
 
-class FbUrlFormatter(url: String) {
+class FbUrlFormatter(url: String, decode: Boolean = false) {
     private val queries = mutableMapOf<String, String>()
     private val cleaned: String
 
@@ -19,8 +19,10 @@ class FbUrlFormatter(url: String) {
         else {
             var cleanedUrl = url
             discardable.forEach { cleanedUrl = cleanedUrl.replace(it, "", true) }
-            val changed = cleanedUrl != url //note that discardables strip away the first ?
-            decoder.forEach { (k, v) -> cleanedUrl = cleanedUrl.replace(k, v, true) }
+            val changed = cleanedUrl != url //note that discardables strip away the first '?'
+            converter.forEach { (k, v) -> cleanedUrl = cleanedUrl.replace(k, v, true) }
+            if (decode)
+                decoder.forEach { (k, v) -> cleanedUrl = cleanedUrl.replace(k, v, true) }
             val qm = cleanedUrl.indexOf(if (changed) "&" else "?")
             if (qm > -1) {
                 cleanedUrl.substring(qm + 1).split("&").forEach {
@@ -83,15 +85,18 @@ class FbUrlFormatter(url: String) {
                 "%60" to "`", "%3B" to ";", "%2F" to "/", "%3F" to "?",
                 "%3A" to ":", "%40" to "@", "%3D" to "=", "%26" to "&",
                 "%24" to "$", "%2B" to "+", "%22" to "\"", "%2C" to ",",
-                "%20" to " ",
-                //css
-                "\\3C " to "<", "\\3E " to ">", "\\23 " to "#", "\\25 " to "%",
-                "\\7B " to "{", "\\7D " to "}", "\\7C " to "|", "\\5C " to "\\",
-                "\\5E " to "^", "\\7E " to "~", "\\5B " to "[", "\\5D " to "]",
-                "\\60 " to "`", "\\3B " to ";", "\\2F " to "/", "\\3F " to "?",
-                "\\3A " to ":", "\\40 " to "@", "\\3D " to "=", "\\26 " to "&",
-                "\\24 " to "$", "\\2B " to "+", "\\22 " to "\"", "\\2C " to ",",
-                "\\20 " to " "
+                "%20" to " "
+        )
+
+        @JvmStatic
+        val converter = mapOf(
+                "\\3C " to "%3C", "\\3E " to "%3E", "\\23 " to "%23", "\\25 " to "%25",
+                "\\7B " to "%7B", "\\7D " to "%7D", "\\7C " to "%7C", "\\5C " to "%5C",
+                "\\5E " to "%5E", "\\7E " to "%7E", "\\5B " to "%5B", "\\5D " to "%5D",
+                "\\60 " to "%60", "\\3B " to "%3B", "\\2F " to "%2F", "\\3F " to "%3F",
+                "\\3A " to "%3A", "\\40 " to "%40", "\\3D " to "%3D", "\\26 " to "%26",
+                "\\24 " to "%24", "\\2B " to "%2B", "\\22 " to "%22", "\\2C " to "%2C",
+                "\\20 " to "%20"
         )
     }
 }
