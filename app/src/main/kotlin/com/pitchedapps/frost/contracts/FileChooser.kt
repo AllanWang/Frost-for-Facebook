190aeaf39617843a061c45ee181847e24c2860ce
Allan Wang <me@allanwang.ca>
2017-10-13 19:55:57 -0400

    Remove kau mediapicker

--- app/src/main/kotlin/com/pitchedapps/frost/contracts/FileChooser.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/contracts/FileChooser.kt
@@ -5,10 +5,8 @@ import android.content.Intent
 import android.net.Uri
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
-import ca.allanwang.kau.mediapicker.kauLaunchMediaPicker
-import ca.allanwang.kau.mediapicker.kauOnMediaPickerResult
-import com.pitchedapps.frost.activities.ImagePickerActivity
-import com.pitchedapps.frost.activities.VideoPickerActivity
+import ca.allanwang.kau.utils.string
+import com.pitchedapps.frost.R
 import com.pitchedapps.frost.utils.L
 
 /**
@@ -32,18 +30,18 @@ class FileChooserDelegate : FileChooserContract {
 
     override fun Activity.openMediaPicker(filePathCallback: ValueCallback<Array<Uri>>, fileChooserParams: WebChromeClient.FileChooserParams) {
         this@FileChooserDelegate.filePathCallback = filePathCallback
-        val isVideo = fileChooserParams.acceptTypes.firstOrNull() == "video/*"
-        kauLaunchMediaPicker(if (isVideo) VideoPickerActivity::class.java else ImagePickerActivity::class.java, MEDIA_CHOOSER_RESULT)
+        val intent = Intent()
+        intent.type = fileChooserParams.acceptTypes.firstOrNull()
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, string(R.string.pick_image)), MEDIA_CHOOSER_RESULT)
     }
 
     override fun Activity.onActivityResultWeb(requestCode: Int, resultCode: Int, intent: Intent?): Boolean {
         L.d("FileChooser On activity results web $requestCode")
         if (requestCode != MEDIA_CHOOSER_RESULT) return false
-        val results = kauOnMediaPickerResult(resultCode, intent).map { it.uri }.toTypedArray()
-        L.i("FileChooser result ${results.contentToString()}")
-        //proper path content://com.android.providers.media.documents/document/image%3A36341
-        L.d("FileChooser Callback received; ${filePathCallback != null}")
-        filePathCallback?.onReceiveValue(results)
+        val data = intent?.data
+        if (data != null)
+            filePathCallback?.onReceiveValue(arrayOf(data))
         filePathCallback = null
         return true
     }
