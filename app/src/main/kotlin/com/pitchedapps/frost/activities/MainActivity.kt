4519562d06fec33c699c6c4c2567d5e3298dd927
Allan Wang <me@allanwang.ca>
2017-10-25 12:41:36 -0400

    Optimize

--- app/src/main/kotlin/com/pitchedapps/frost/activities/MainActivity.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/activities/MainActivity.kt
@@ -5,7 +5,6 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.Point
 import android.graphics.PointF
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
