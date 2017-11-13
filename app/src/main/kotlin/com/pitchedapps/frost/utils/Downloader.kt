f680337b1e90c007b2a76b594eb196127b3bc6f6
Allan Wang <me@allanwang.ca>
2017-11-13 04:58:41 -0500

    Check for download manager before download attempt

--- app/src/main/kotlin/com/pitchedapps/frost/utils/Downloader.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/utils/Downloader.kt
@@ -8,10 +8,15 @@ import android.os.Environment
 import android.webkit.URLUtil
 import ca.allanwang.kau.permissions.PERMISSION_WRITE_EXTERNAL_STORAGE
 import ca.allanwang.kau.permissions.kauRequestPermissions
+import ca.allanwang.kau.utils.isAppEnabled
 import ca.allanwang.kau.utils.string
 import com.pitchedapps.frost.R
 import com.pitchedapps.frost.dbflow.loadFbCookie
 import com.pitchedapps.frost.facebook.USER_AGENT_BASIC
+import android.support.v4.content.ContextCompat.startActivity
+import android.content.Intent
+import android.content.ActivityNotFoundException
+import ca.allanwang.kau.utils.showAppInfo
 
 
 /**
@@ -37,6 +42,16 @@ fun Context.frostDownload(uri: Uri?,
     L.d("Received download request", "Download $uri")
     if (uri.scheme != "http" && uri.scheme != "https")
         return L.e("Invalid download attempt", uri.toString())
+    if (!isAppEnabled(DOWNLOAD_MANAGER_PACKAGE)) {
+        materialDialogThemed {
+            title(R.string.no_download_manager)
+            content(R.string.no_download_manager_desc)
+            positiveText(R.string.kau_yes)
+            onPositive { _, _ -> showAppInfo(DOWNLOAD_MANAGER_PACKAGE) }
+            negativeText(R.string.kau_no)
+        }
+        return
+    }
     kauRequestPermissions(PERMISSION_WRITE_EXTERNAL_STORAGE) { granted, _ ->
         if (!granted) return@kauRequestPermissions
         val request = DownloadManager.Request(uri)
@@ -54,3 +69,5 @@ fun Context.frostDownload(uri: Uri?,
         dm.enqueue(request)
     }
 }
+
+private const val DOWNLOAD_MANAGER_PACKAGE = "com.android.providers.downloads"
