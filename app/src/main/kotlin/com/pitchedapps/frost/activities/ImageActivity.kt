8cff8f7e6add9ad74bad7ca52718b4828b54a509
Allan Wang <me@allanwang.ca>
2017-11-13 04:29:38 -0500

    Fix unit test and add null check for images, resolves #458

--- app/src/main/kotlin/com/pitchedapps/frost/activities/ImageActivity.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/activities/ImageActivity.kt
@@ -76,6 +76,7 @@ class ImageActivity : KauBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        intent?.extras ?: return finish()
         L.i("Displaying image", imageUrl)
         val layout = if (!text.isNullOrBlank()) R.layout.activity_image else R.layout.activity_image_textless
         setContentView(layout)
