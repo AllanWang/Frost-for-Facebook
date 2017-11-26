41d27aeb517eb8f9a870a7ae0292761e6c0dd92d
Allan Wang <me@allanwang.ca>
2017-11-26 16:49:09 -0500

    Misc (#513)
    * Split content call from adding call
    
    * Comment out reactive network code
    
    * Remove autoplay settings, fixes #500

--- app/src/main/kotlin/com/pitchedapps/frost/activities/BaseActivity.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/activities/BaseActivity.kt
@@ -4,18 +4,8 @@ import android.content.res.Configuration
 import android.os.Bundle
 import ca.allanwang.kau.internal.KauBaseActivity
 import ca.allanwang.kau.searchview.SearchViewHolder
-import ca.allanwang.kau.utils.finishSlideOut
-import com.github.pwittchen.reactivenetwork.library.rx2.Connectivity
-import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork
-import com.pitchedapps.frost.R
 import com.pitchedapps.frost.contracts.VideoViewHolder
-import com.pitchedapps.frost.utils.L
-import com.pitchedapps.frost.utils.Prefs
-import com.pitchedapps.frost.utils.materialDialogThemed
 import com.pitchedapps.frost.utils.setFrostTheme
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 
 /**
  * Created by Allan Wang on 2017-06-12.
@@ -38,43 +28,43 @@ abstract class BaseActivity : KauBaseActivity() {
         super.onCreate(savedInstanceState)
         if (this !is WebOverlayActivityBase) setFrostTheme()
     }
-
-    private var networkDisposable: Disposable? = null
-    private var networkConsumer: ((Connectivity) -> Unit)? = null
-
-    fun setNetworkObserver(consumer: (connectivity: Connectivity) -> Unit) {
-        this.networkConsumer = consumer
-    }
-
-    private fun observeNetworkConnectivity() {
-        val consumer = networkConsumer ?: return
-        networkDisposable = ReactiveNetwork.observeNetworkConnectivity(applicationContext)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { connectivity: Connectivity ->
-                    connectivity.apply {
-                        L.d("Network connectivity changed: isAvailable: $isAvailable isRoaming: $isRoaming")
-                        consumer(connectivity)
-                    }
-                }
-    }
-
-    private fun disposeNetworkConnectivity() {
-        if (networkDisposable?.isDisposed == false)
-            networkDisposable?.dispose()
-        networkDisposable = null
-    }
-
-    override fun onResume() {
-        super.onResume()
-//        disposeNetworkConnectivity()
-//        observeNetworkConnectivity()
-    }
-
-    override fun onPause() {
-        super.onPause()
-//        disposeNetworkConnectivity()
-    }
+//
+//    private var networkDisposable: Disposable? = null
+//    private var networkConsumer: ((Connectivity) -> Unit)? = null
+//
+//    fun setNetworkObserver(consumer: (connectivity: Connectivity) -> Unit) {
+//        this.networkConsumer = consumer
+//    }
+//
+//    private fun observeNetworkConnectivity() {
+//        val consumer = networkConsumer ?: return
+//        networkDisposable = ReactiveNetwork.observeNetworkConnectivity(applicationContext)
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe { connectivity: Connectivity ->
+//                    connectivity.apply {
+//                        L.d("Network connectivity changed: isAvailable: $isAvailable isRoaming: $isRoaming")
+//                        consumer(connectivity)
+//                    }
+//                }
+//    }
+//
+//    private fun disposeNetworkConnectivity() {
+//        if (networkDisposable?.isDisposed == false)
+//            networkDisposable?.dispose()
+//        networkDisposable = null
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+////        disposeNetworkConnectivity()
+////        observeNetworkConnectivity()
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+////        disposeNetworkConnectivity()
+//    }
 
 
     override fun onStop() {
