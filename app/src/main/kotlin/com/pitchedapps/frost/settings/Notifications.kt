1424535fc27f4b342902aebacb11c8079516c219
Allan Wang <me@allanwang.ca>
2018-03-04 23:21:42 -0500

    Hide notif settings that no longer work with channels

--- app/src/main/kotlin/com/pitchedapps/frost/settings/Notifications.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/settings/Notifications.kt
@@ -1,14 +1,15 @@
 package com.pitchedapps.frost.settings
 
-import android.app.NotificationChannel
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.media.RingtoneManager
 import android.net.Uri
+import android.os.Build
+import android.provider.Settings
 import ca.allanwang.kau.kpref.activity.KPrefAdapterBuilder
 import ca.allanwang.kau.kpref.activity.items.KPrefText
 import ca.allanwang.kau.utils.minuteToText
 import ca.allanwang.kau.utils.string
-import com.pitchedapps.frost.BuildConfig
 import com.pitchedapps.frost.R
 import com.pitchedapps.frost.activities.SettingsActivity
 import com.pitchedapps.frost.services.fetchNotifications
@@ -22,6 +23,7 @@ import com.pitchedapps.frost.views.Keywords
 /**
  * Created by Allan Wang on 2017-06-29.
  */
+@SuppressLint("InlinedApi")
 fun SettingsActivity.getNotificationPrefs(): KPrefAdapterBuilder.() -> Unit = {
 
     text(R.string.notification_frequency, Prefs::notificationFreq, { Prefs.notificationFreq = it }) {
@@ -92,48 +94,59 @@ fun SettingsActivity.getNotificationPrefs(): KPrefAdapterBuilder.() -> Unit = {
         enabler = Prefs::notificationsInstantMessages
     }
 
-    checkbox(R.string.notification_sound, Prefs::notificationSound, {
-        Prefs.notificationSound = it
-        reloadByTitle(R.string.notification_ringtone,
-                R.string.message_ringtone)
-    })
-
-    fun KPrefText.KPrefTextContract<String>.ringtone(code: Int) {
-        enabler = Prefs::notificationSound
-        textGetter = {
-            if (it.isBlank()) string(R.string.kau_default)
-            else RingtoneManager.getRingtone(this@getNotificationPrefs, Uri.parse(it))
-                    ?.getTitle(this@getNotificationPrefs)
-                    ?: "---" //todo figure out why this happens
-        }
-        onClick = {
-            val intent = Intent(RingtoneManager.ACTION_RINGTONE_PICKER).apply {
-                putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, string(R.string.select_ringtone))
-                putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false)
-                putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_DEFAULT, true)
-                putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_NOTIFICATION)
-                if (item.pref.isNotBlank())
-                    putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, Uri.parse(item.pref))
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        plainText(R.string.notification_channel) {
+            descRes = R.string.notification_channel_desc
+            onClick = {
+                val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                        .putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+                startActivity(intent)
             }
-            startActivityForResult(intent, code)
         }
+    } else {
+        checkbox(R.string.notification_sound, Prefs::notificationSound, {
+            Prefs.notificationSound = it
+            reloadByTitle(R.string.notification_ringtone,
+                    R.string.message_ringtone)
+        })
+
+        fun KPrefText.KPrefTextContract<String>.ringtone(code: Int) {
+            enabler = Prefs::notificationSound
+            textGetter = {
+                if (it.isBlank()) string(R.string.kau_default)
+                else RingtoneManager.getRingtone(this@getNotificationPrefs, Uri.parse(it))
+                        ?.getTitle(this@getNotificationPrefs)
+                        ?: "---" //todo figure out why this happens
+            }
+            onClick = {
+                val intent = Intent(RingtoneManager.ACTION_RINGTONE_PICKER).apply {
+                    putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, string(R.string.select_ringtone))
+                    putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false)
+                    putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_DEFAULT, true)
+                    putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_NOTIFICATION)
+                    if (item.pref.isNotBlank())
+                        putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, Uri.parse(item.pref))
+                }
+                startActivityForResult(intent, code)
+            }
+        }
+
+        text(R.string.notification_ringtone, Prefs::notificationRingtone,
+                { Prefs.notificationRingtone = it }) {
+            ringtone(SettingsActivity.REQUEST_NOTIFICATION_RINGTONE)
+        }
+
+        text(R.string.message_ringtone, Prefs::messageRingtone,
+                { Prefs.messageRingtone = it }) {
+            ringtone(SettingsActivity.REQUEST_MESSAGE_RINGTONE)
+        }
+
+        checkbox(R.string.notification_vibrate, Prefs::notificationVibrate,
+                { Prefs.notificationVibrate = it })
+
+        checkbox(R.string.notification_lights, Prefs::notificationLights,
+                { Prefs.notificationLights = it })
     }
-
-    text(R.string.notification_ringtone, Prefs::notificationRingtone,
-            { Prefs.notificationRingtone = it }) {
-        ringtone(SettingsActivity.REQUEST_NOTIFICATION_RINGTONE)
-    }
-
-    text(R.string.message_ringtone, Prefs::messageRingtone,
-            { Prefs.messageRingtone = it }) {
-        ringtone(SettingsActivity.REQUEST_MESSAGE_RINGTONE)
-    }
-
-    checkbox(R.string.notification_vibrate, Prefs::notificationVibrate,
-            { Prefs.notificationVibrate = it })
-
-    checkbox(R.string.notification_lights, Prefs::notificationLights,
-            { Prefs.notificationLights = it })
 
     plainText(R.string.notification_fetch_now) {
         descRes = R.string.notification_fetch_now_desc
