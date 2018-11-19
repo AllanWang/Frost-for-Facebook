935b19f54b2c9b2c16998534914b305e1b1152b2
Allan Wang <me@allanwang.ca>
2018-11-18 20:33:04 -0500

    Fix regex to support personal messages, resolves #1180

--- app/src/main/kotlin/com/pitchedapps/frost/facebook/FbRegex.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/facebook/FbRegex.kt
@@ -18,15 +18,15 @@ val FB_REV_MATCHER: Regex by lazy { Regex("\"app_version\":\"(.*?)\"") }
 /**
  * Matches user id from cookie
  */
-val FB_USER_MATCHER: Regex by lazy { Regex("c_user=([0-9]*);") }
+val FB_USER_MATCHER: Regex = Regex("c_user=([0-9]*);")
 
-val FB_EPOCH_MATCHER: Regex by lazy { Regex(":([0-9]+)") }
-val FB_NOTIF_ID_MATCHER: Regex by lazy { Regex("notif_([0-9]+)") }
-val FB_MESSAGE_NOTIF_ID_MATCHER: Regex by lazy { Regex("(?:thread|user)_fbid_([0-9]+)") }
-val FB_CSS_URL_MATCHER: Regex by lazy { Regex("url\\([\"|']?(.*?)[\"|']?\\)") }
-val FB_JSON_URL_MATCHER: Regex by lazy { Regex("\"(http.*?)\"") }
-val FB_IMAGE_ID_MATCHER: Regex by lazy { Regex("fbcdn.*?/[0-9]+_([0-9]+)_") }
-val FB_REDIRECT_URL_MATCHER: Regex by lazy { Regex("url=(.*?fbcdn.*?)\"") }
+val FB_EPOCH_MATCHER: Regex = Regex(":([0-9]+)")
+val FB_NOTIF_ID_MATCHER: Regex = Regex("notif_([0-9]+)")
+val FB_MESSAGE_NOTIF_ID_MATCHER: Regex = Regex("(?:thread|user)_fbid_([0-9]+)")
+val FB_CSS_URL_MATCHER: Regex = Regex("url\\([\"|']?(.*?)[\"|']?\\)")
+val FB_JSON_URL_MATCHER: Regex = Regex("\"(http.*?)\"")
+val FB_IMAGE_ID_MATCHER: Regex = Regex("fbcdn.*?/[0-9]+_([0-9]+)_")
+val FB_REDIRECT_URL_MATCHER: Regex = Regex("url=(.*?fbcdn.*?)\"")
 
 operator fun MatchResult?.get(groupIndex: Int) = this?.groupValues?.get(groupIndex)
 
