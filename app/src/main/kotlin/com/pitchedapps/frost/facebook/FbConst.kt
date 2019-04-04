5381c9b1c4fc2b9669436f1a9a789410bfae256b
Allan Wang <me@allanwang.ca>
2019-04-04 15:53:47 -0400

    Add extra forward slash

--- app/src/main/kotlin/com/pitchedapps/frost/facebook/FbConst.kt
+++ app/src/main/kotlin/com/pitchedapps/frost/facebook/FbConst.kt
@@ -22,7 +22,7 @@ package com.pitchedapps.frost.facebook
 
 const val FACEBOOK_COM = "facebook.com"
 const val FBCDN_NET = "fbcdn.net"
-const val HTTPS_FACEBOOK_COM = "https://$FACEBOOK_COM"
+const val HTTPS_FACEBOOK_COM = "https://$FACEBOOK_COM/"
 const val FB_URL_BASE = "https://m.$FACEBOOK_COM/"
 fun profilePictureUrl(id: Long) = "https://graph.facebook.com/$id/picture?type=large"
 const val FB_LOGIN_URL = "${FB_URL_BASE}login"
