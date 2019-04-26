20b1014de5dd31b79aec68101d5f00db0a270d59
Allan Wang <me@allanwang.ca>
2019-04-25 23:53:00 -0700

    Echo versions

--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-export ANDROID_HOME="/opt/android-sdk-linux"
-
 printf "Starting script\n"
 chmod +x gradlew
-./gradlew --quiet androidGitVersion
-./gradlew lintReleaseTest testReleaseUnitTest assembleReleaseTest
+
+nodejs -v
+echo $ANDROID_HOME
+echo "Hello"
+./gradlew -v
+
+#./gradlew --quiet androidGitVersion
+#./gradlew lintReleaseTest testReleaseUnitTest assembleReleaseTest
