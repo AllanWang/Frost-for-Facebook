93875922da5d4193f8097c4abd35fcc67262bb67
Allan Wang <me@allanwang.ca>
2019-05-01 12:43:32 -0700

    Add double brackets

--- files/translation_migration.sh
+++ files/translation_migration.sh
@@ -6,7 +6,7 @@ cd ..
 
 current=${PWD##*/}
 
-if [ "$current" != "$MODULE" ]; then
+if [[ "$current" != "$MODULE" ]]; then
     echo "Not in $MODULE";
 else
     # DANGEROUS! Removes all files matching regex
