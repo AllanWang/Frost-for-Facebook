7903d260585133f37d3e14384ce5c15e797f0038
TacoTheDank <SkytkRSfan3895@gmail.com>
2019-08-08 18:48:45 -0400

    Update dependency changelog links

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,12 +2,13 @@ object Versions {
 
     const val targetSdk = 28
 
+    // https://developer.android.com/jetpack/androidx/releases/biometric
     const val andxBiometric = "1.0.0-alpha04"
 
-    // https://mvnrepository.com/artifact/org.apache.commons/commons-text
+    // https://github.com/apache/commons-text/blob/master/RELEASE-NOTES.txt
     // Updates blocked due to javax.script dependency
     const val apacheCommonsText = "1.4"
-    // https://github.com/Raizlabs/DBFlow/releases
+    // https://github.com/agrosner/DBFlow/releases
     const val dbflow = "4.2.4"
     // https://github.com/brianwernick/ExoMedia/releases
     const val exoMedia = "4.3.0"
@@ -15,24 +16,24 @@ object Versions {
     const val koin = "2.0.1"
     // https://github.com/mockk/mockk/releases
     const val mockk = "1.9.3"
-    // https://mvnrepository.com/artifact/androidx.core/core-ktx?repo=google
+    // https://developer.android.com/jetpack/androidx/releases/core
     const val ktx = "1.0.2"
 
-    // https://github.com/FasterXML/jackson-core/releases
+    // https://github.com/FasterXML/jackson-core/blob/master/release-notes/VERSION-2.x
     const val jackson = "2.9.8"
-    // https://github.com/jhy/jsoup/releases
+    // https://github.com/jhy/jsoup/blob/master/CHANGES
     const val jsoup = "1.11.3"
-    // https://github.com/square/leakcanary/releases
+    // https://square.github.io/leakcanary/changelog/
     const val leakCanary = "1.6.2"
     // https://github.com/zsmb13/MaterialDrawerKt/releases
     const val materialDrawerKt = "2.0.1"
-    // https://github.com/square/okhttp/releases
+    // https://github.com/square/okhttp/blob/master/CHANGELOG.md
     const val okhttp = "3.14.1"
     // https://developer.android.com/jetpack/androidx/releases/room
     const val room = "2.1.0"
-    // http://robolectric.org/getting-started/
+    // https://github.com/robolectric/robolectric/releases/
     const val roboelectric = "4.2"
-    // https://github.com/davemorrissey/subsampling-scale-image-view#quick-start
+    // https://github.com/davemorrissey/subsampling-scale-image-view/releases
     const val scaleImageView = "3.10.0"
     // https://github.com/srs/gradle-node-plugin/releases
     const val nodeGradle = "1.3.1"
