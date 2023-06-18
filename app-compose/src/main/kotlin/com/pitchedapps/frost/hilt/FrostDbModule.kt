3c0a8b3fb96324f3ccb03c5601220b8c4288298b
Allan Wang <me@allanwang.ca>
2023-06-18 15:27:30 -0700

    Create accounts sq

---
+++ app-compose/src/main/kotlin/com/pitchedapps/frost/hilt/FrostDbModule.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023 Allan Wang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.pitchedapps.frost.hilt
+
+import android.content.Context
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import com.pitchedapps.frost.db.FrostDb
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FrostDbModule {
+  @Singleton
+  fun frostDb(@ApplicationContext context: Context): FrostDb {
+    val driver: SqlDriver = AndroidSqliteDriver(FrostDb.Schema, context, "frost.db")
+    return FrostDb(driver)
+  }
+}
