48339b8a157358549fb9199d1e25f76a93afc446
Allan Wang <me@allanwang.ca>
2023-06-17 21:43:39 -0700

    Add base application and activity

---
+++ app-compose/src/main/kotlin/com/pitchedapps/frost/FrostApp.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018 Allan Wang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.pitchedapps.frost
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import android.util.Log
+import com.google.common.flogger.FluentLogger
+import dagger.hilt.android.HiltAndroidApp
+import java.util.Random
+import javax.inject.Inject
+
+@HiltAndroidApp
+class FrostApp : Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+
+    if (BuildConfig.DEBUG) {
+      registerActivityLifecycleCallbacks(
+        object : ActivityLifecycleCallbacks {
+          override fun onActivityPaused(activity: Activity) {}
+          override fun onActivityResumed(activity: Activity) {}
+          override fun onActivityStarted(activity: Activity) {}
+
+          override fun onActivityDestroyed(activity: Activity) {
+            logger.atFine().log("Activity %s destroyed", activity.localClassName)
+          }
+
+          override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+
+          override fun onActivityStopped(activity: Activity) {}
+
+          override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            logger.atFine().log("Activity %s created", activity.localClassName)
+          }
+        }
+      )
+    }
+  }
+
+  companion object {
+    private val logger = FluentLogger.forEnclosingClass()
+  }
+}
---
+++ app-compose/src/main/kotlin/com/pitchedapps/frost/StartActivity.kt
@@ -0,0 +1,8 @@
+package com.pitchedapps.frost
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class StartActivity : AppCompatActivity() {
+}
